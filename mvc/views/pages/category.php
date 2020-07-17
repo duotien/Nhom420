03cea7cb6ab55fe217a0ca157cfee2b31f4426cd
Tien Duong <tienmum@gmail.com>
2020-07-17 14:16:51 +0700

    changing root and cleaning up

--- mvc/views/pages/category.php
+++ mvc/views/pages/category.php
@@ -1,10 +1,10 @@
 <?php
-foreach($data['Array'] as $row)
+foreach ($data['Array'] as $row)
 {
 ?>
     <div class="n420-product-box">
         <a href="<?php echo $row['id'] . ".php" ?>">
-            <img class="n420-product-image" src="<?php echo $row['img_path'] ?>" alt="" />
+            <img class="n420-product-image" src="public/<?php echo $row['img_path'] ?>" alt="" />
             <!--img class="n420-product-icon" src="" alt=""-->
             <div class="n420-product-name-price">
                 <b><?php echo $row['name'] ?></b>
