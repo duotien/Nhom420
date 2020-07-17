a7538072d72ce851a707b85714d4899aa89bbb5b
Tien Duong <tienmum@gmail.com>
2020-07-18 00:07:14 +0700

    add productdetail page

---
+++ mvc/views/pages/productdetail.php
@@ -0,0 +1,12 @@
+<html>
+<br><br><br>
+----------------------------------<br>
+<?php
+    echo "sản phẩm có id là: ".$data["id"];
+?>
+<br>
+Thả HTML của chi tiết sản phẩm ở thư mục:<br>
+mvc\views\pages\productdetail.php<br>
+----------------------------------<br>
+<br><br><br>
+</html>
