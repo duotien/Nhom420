8c28c0bef57449e927bf3d935a2246fdf0b6c318
Tien Duong <tienmum@gmail.com>
2020-07-17 12:07:52 +0700

    cleaning up

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 ﻿<?php
 session_start();
-
 require_once "./mvc/Bridge.php";
-
 $myApp = new App();
-
 //include_once('mvc/views/blocks/header.php');
 //include_once('mvc/views/blocks/footer.php');
-?>
