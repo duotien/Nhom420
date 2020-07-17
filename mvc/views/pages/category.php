e395a357741b2768e23bf3498dcd2236861614ea
Tien Duong <tienmum@gmail.com>
2020-07-17 13:24:37 +0700

    add pages/category.php

---
+++ mvc/views/pages/category.php
@@ -0,0 +1,16 @@
+<?php
+foreach($data['Array'] as $row)
+{
+?>
+    <div class="n420-product-box">
+        <a href="<?php echo $row['id'] . ".php" ?>">
+            <img class="n420-product-image" src="<?php echo $row['img_path'] ?>" alt="" />
+            <!--img class="n420-product-icon" src="" alt=""-->
+            <div class="n420-product-name-price">
+                <b><?php echo $row['name'] ?></b>
+                <span><?php echo $row['price'] ?></span>
+            </div>
+        </a>
+    </div>
+<?php
+}
