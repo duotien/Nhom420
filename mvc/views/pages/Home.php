bdc1193b3ec981be9421c089742fea544244eb1c
Tien Duong <tienmum@gmail.com>
2020-07-17 23:41:47 +0700

    add Home.php (page), insert html here

---
+++ mvc/views/pages/Home.php
@@ -0,0 +1,8 @@
+<html>
+<br><br><br>
+----------------------------------<br>
+Thả HTML của Trang chủ index ở thư mục:<br>
+mvc\views\pages\Home.php<br>
+----------------------------------<br>
+<br><br><br>
+</html>
