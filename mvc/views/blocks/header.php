e3c29bafbf96bee8f6890a41e968323ba106c849
Duong Tien <4401104193@student.hcmue.edu.vn>
2020-07-18 14:32:08 +0700

    fix navigation

--- mvc/views/blocks/header.php
+++ mvc/views/blocks/header.php
@@ -29,31 +29,34 @@
                     <div class="header-menu col-xs-12 col-sm-6 col-md-5">
                         <ul class="main-menu clearfix">
                             <li class="is-child">
-                                <a href="men.html">MEN</a>
+                                <a href="../Product/Men">MEN</a>
                             </li>
                             <li class="is-child">
-                                <a href="">WOMEN</a>
+                                <a href="../Product/Women">WOMEN</a>
                             </li>
                             <li class="is-child">
-                                <a href="">SALE</a>
+                                <a href="../Sale">SALE</a>
                             </li>
                             <li class="is-child">
-                                <a href="">CONTACT</a>
+                                <a href="../Contact">CONTACT</a>
                             </li>
                         </ul>
                     </div>
+
+<!------------------Xử lí phần SEARCH tại đây-->
                     <div class="search-box col-xs-12 col-sm-2 col-md-3">
                         <form class="navbar-form" action="">
                             <div class="input-group">
                               <input type="text" class="form-control" placeholder="Search">
                               <div class="input-group-btn">
-                                <button class="btn btn-default" type="submit">
+                                <button class="btn btn-default" type="submit" >
                                     <i class="fa fa-search" aria-hidden="true"></i>
                                 </button>
                               </div>
                             </div>
                         </form>
                     </div>
+<!------------------Xử lí phần SEARCH tại đây-->
                     <div class="group-box col-xs-12 col-sm-2 col-md-2">
                         <a href="cart.html"><i class="fa fa-shopping-cart" aria-hidden="true"></i></a>
                         <a href="login.html"><i class="fa fa-user" aria-hidden="true"></i></a>
