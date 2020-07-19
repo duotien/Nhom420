51b7e315aa57de02e32ad2dd24cca571043e6898
Duong Tien <4401104193@student.hcmue.edu.vn>
2020-07-19 17:13:02 +0700

    rename cart.html to cart.php

---
+++ mvc/views/pages/cart.php
@@ -0,0 +1,87 @@
+<hr>
+        <section class="jumbotron text-center">
+            <div class="container">
+                <h1 class="jumbotron-heading">YOUR SHOPPING CART</h1>
+             </div>
+        </section>
+        
+        <div class="container mb-4">
+            <div class="row">
+                <div class="col-12">
+                    <div class="table-responsive">
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th scope="col"> </th>
+                                    <th scope="col">Product</th>
+                                    <th scope="col">Available</th>
+                                    <th scope="col" class="text-center">Quantity</th>
+                                    <th scope="col" class="text-right">Price</th>
+                                    <th> </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td><img src="./images/placeHolder.jpg" /> </td>
+                                    <td>Product Name</td>
+                                    <td>In stock</td>
+                                    <td><input class="form-control" type="text" value="1" /></td>
+                                    <td class="text-right">124.000đ</td>
+                                    <td class="text-right"><button class="btn btn-sm btn-danger"><i class="fa fa-trash"></i> </button> </td>
+                                </tr>
+                                <tr>
+                                    <td><img src="./images/placeHolder.jpg" /> </td>
+                                    <td>Product Name</td>
+                                    <td>In stock</td>
+                                    <td><input class="form-control" type="text" value="1" /></td>
+                                    <td class="text-right">124.000đ</td>
+                                    <td class="text-right"><button class="btn btn-sm btn-danger"><i class="fa fa-trash"></i> </button> </td>
+                                </tr>
+                                <tr>
+                                    <td><img src="./images/placeHolder.jpg" /> </td>
+                                    <td>Product Name/td>
+                                    <td>In stock</td>
+                                    <td><input class="form-control" type="text" value="1" /></td>
+                                    <td class="text-right">124.000đ</td>
+                                    <td class="text-right"><button class="btn btn-sm btn-danger"><i class="fa fa-trash"></i> </button> </td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td>Sub-Total</td>
+                                    <td class="text-right">372.000đ</td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td>Shipping</td>
+                                    <td class="text-right">25.000đ</td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td><strong>Total</strong></td>
+                                    <td class="text-right"><strong>397.000đ</strong></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div class="col mb-2">
+                    <div class="row">
+                        <!-- <div class="col-sm-12  col-md-6">
+                            <button class="btn btn-block btn-light">Continue Shopping</button>
+                        </div> -->
+                        <div class="col-sm-12 col-md-6 text-right">
+                            <button class="btn btn-lg btn-block btn-success text-uppercase">Checkout</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
