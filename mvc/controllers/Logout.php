935d7bd02fb1ed39af0bc8a3f8cc6522c2a4c3ce
Duong Tien <4401104193@student.hcmue.edu.vn>
2020-07-20 09:34:56 +0700

    minor change

--- mvc/controllers/Logout.php
+++ mvc/controllers/Logout.php
@@ -1,9 +1,16 @@
 <?php
 class Logout extends Controller
 {
+    public $LogoutModel;
+
+    function __construct()
+    {
+        $this->LogoutModel = $this->getModel("UserModel");
+    }
+
     function showDefault()
     {
         header("location: http://localhost/Nhom420");
-        unset($_SESSION["user"]);
+        $this->LogoutModel->logout();
     }
 };
